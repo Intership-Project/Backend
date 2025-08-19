21fff36e6864a9fe0539d3c4269b14a05586ae85
W2-83851-Shreya <shreyachangle@gmail.com>
2025-08-19 21:53:42 +0530

    get role by id and delete role

--- routes/role.js
+++ routes/role.js
@@ -26,5 +26,88 @@ router.post('/register', async (request, response) => {
   }
 })
 
+// GET All Roles
+router.get('/', async (request, response) => {
+  try {
+    const statement = `
+      SELECT role_id, rolename
+      FROM Role
+    `
+
+    const [rows] = await db.execute(statement)
+
+    response.send(utils.createSuccess(rows))
+  } catch (ex) {
+    response.send(utils.createError(ex))
+  }
+})
+
+// GET Role by ID
+router.get('/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const statement = `
+      SELECT role_id, rolename
+      FROM Role
+      WHERE role_id = ?
+    `
+
+    const [rows] = await db.execute(statement, [id])
+
+    if (rows.length === 0) {
+      response.send(utils.createError('Role not found'))
+    } else {
+      response.send(utils.createSuccess(rows[0]))
+    }
+  } catch (ex) {
+    response.send(utils.createError(ex))
+  }
+})
+
+// GET Role by ID
+router.get('/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const statement = `
+      SELECT role_id, rolename
+      FROM Role
+      WHERE role_id = ?
+    `
+
+    const [rows] = await db.execute(statement, [id])
+
+    if (rows.length === 0) {
+      response.send(utils.createError('Role not found'))
+    } else {
+      response.send(utils.createSuccess(rows[0]))
+    }
+  } catch (ex) {
+    response.send(utils.createError(ex))
+  }
+})
+
+// DELETE Role
+router.delete('/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const statement = `
+      DELETE FROM Role
+      WHERE role_id = ?
+    `
+
+    const [result] = await db.execute(statement, [id])
+
+    if (result.affectedRows === 0) {
+      response.send(utils.createError('Role not found or already deleted'))
+    } else {
+      response.send(utils.createSuccess(`Role with ID ${id} deleted successfully`))
+    }
+  } catch (ex) {
+    response.send(utils.createError(ex))
+  }
+})
 
 module.exports = router
