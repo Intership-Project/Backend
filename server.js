351601bb322f816929e2ef0b5ab087f66081d233
W2-83851-Shreya <shreyachangle@gmail.com>
2025-08-19 09:46:54 +0530

    Login and Registration for Admin

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const db = require('./db')
+const utils = require('./utils')
+const morgan = require('morgan')
+
+// create app
+const app = express()
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+
+const adminRoute = require('./routes/admin')
+
+
+// use routes
+
+app.use('/admin', adminRoute)
+
+
+// default route
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+// start server
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
+
+
+
+
+
+
+
