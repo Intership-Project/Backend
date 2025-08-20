93d4f80c5ea3af14bf6e69c611b95e57877009e1
W2-83851-Shreya <shreyachangle@gmail.com>
2025-08-20 09:34:57 +0530

    get batch by ID

--- routes/batch.js
+++ routes/batch.js
@@ -32,4 +32,20 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET Batch by ID
+router.get('/:batch_id', async (req, res) => {
+  const { batch_id } = req.params
+  try {
+    const statement = `SELECT Batch.batch_id,Batch.batchname,Course.course_id,Course.coursename FROM Batch INNER JOIN Course ON Batch.course_id = Course.course_id WHERE Batch.batch_id = ?;`
+    const [rows] = await db.execute(statement, [batch_id])
+    if (rows.length === 0) {
+      res.send(utils.createError('Batch not found'))
+    } else {
+      res.send(utils.createSuccess(rows[0]))
+    }
+  } catch (ex) {
+    res.send(utils.createError(ex))
+  }
+})
+
 module.exports = router
