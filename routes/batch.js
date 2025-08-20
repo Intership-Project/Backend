56c6fedc497ebcf5d7eb3a044b55ba23adca9f99
W3-Madhura-84087 <madhura.bboralkar1999@gmail.com>
2025-08-20 09:46:04 +0530

    batch update

--- routes/batch.js
+++ routes/batch.js
@@ -40,7 +40,6 @@ router.get('/:batch_id', async (req, res) => {
     try {
       const statement = 'SELECT Batch.batch_id,Batch.batchname,Course.course_id,Course.coursename FROM Batch INNER JOIN Course ON Batch.course_id = Course.course_id WHERE Batch.batch_id = ?;'
 
-        
       const [rows] = await db.execute(statement, [batch_id])
       if (rows.length === 0) {
         res.send(utils.createError('Batch not found'))
@@ -51,5 +50,28 @@ router.get('/:batch_id', async (req, res) => {
       res.send(utils.createError(ex))
     }
   })
+  // UPDATE Batch
+router.put('/:batch_id', async (req, res) => {
+    const { batch_id } = req.params
+    const { batchname, course_id } = req.body
+  
+    try {
+      const statement = `
+        UPDATE Batch
+        SET batchname = ?, course_id = ?
+        WHERE batch_id = ?
+      `
+      const [result] = await db.execute(statement, [batchname, course_id, batch_id])
+  
+      if (result.affectedRows === 0) {
+        res.send(utils.createError('Batch not found or not updated'))
+      } else {
+        res.send(utils.createSuccess({ batch_id, batchname, course_id }))
+      }
+    } catch (ex) {
+      res.send(utils.createError(ex))
+    }
+  })
+  
   
 module.exports = router
