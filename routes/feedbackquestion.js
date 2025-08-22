feed68d83be8ac2c955b6f169b16255daa28c765
W2-83851-Shreya <shreyachangle@gmail.com>
2025-08-22 10:29:55 +0530

    get feedbackquestion by feedbacktype

--- routes/feedbackquestion.js
+++ routes/feedbackquestion.js
@@ -57,5 +57,73 @@ router.get('/:feedbackquestion_id', async (req, res) => {
   }
 })
 
+
+// UPDATE FeedbackQuestion
+
+router.put('/:feedbackquestion_id', async (req, res) => {
+  const { feedbackquestion_id } = req.params
+  const { questiontext, feedbacktype_id } = req.body
+  try {
+    if (!questiontext || !feedbacktype_id) {
+      return res.send(utils.createError('questiontext and feedbacktype_id are required'))
+    }
+
+    const statement = `UPDATE FeedbackQuestions SET questiontext = ?, feedbacktype_id = ? WHERE feedbackquestion_id = ? `
+    const [result] = await db.execute(statement, [questiontext, feedbacktype_id, feedbackquestion_id])
+
+    if (result.affectedRows === 0) {
+      res.send(utils.createError('FeedbackQuestion not found'))
+    } else {
+      res.send(utils.createSuccess({
+        feedbackquestion_id,
+        questiontext,
+        feedbacktype_id
+      }))
+    }
+  } catch (ex) {
+    res.send(utils.createError(ex))
+  }
+})
+
+// DELETE FeedbackQuestion
+router.delete('/:feedbackquestion_id', async (req, res) => {
+  const { feedbackquestion_id } = req.params
+  try {
+    const statement = `DELETE FROM FeedbackQuestions WHERE feedbackquestion_id = ?`
+    const [result] = await db.execute(statement, [feedbackquestion_id])
+
+    if (result.affectedRows === 0) {
+      res.send(utils.createError('FeedbackQuestion not found'))
+    } else {
+      res.send(utils.createSuccess(`FeedbackQuestion with id ${feedbackquestion_id} deleted`))
+    }
+  } catch (ex) {
+    if (ex.code === 'ER_ROW_IS_REFERENCED_2') {
+      res.send(utils.createError('Cannot delete: FeedbackQuestion is in use'))
+    } else {
+      res.send(utils.createError(ex))
+    }
+  }
+})
+
+
+//  Get Questions by FeedbackType
+router.get('/feedbacktype/:feedbacktype_id', async (req, res) => {
+  const { feedbacktype_id } = req.params
+  try {
+    const statement = `SELECT feedbackquestion_id, questiontext FROM FeedbackQuestions WHERE feedbacktype_id = ? `
+    const [rows] = await db.execute(statement, [feedbacktype_id])
+
+    if (rows.length === 0) {
+      res.send(utils.createError('No questions found for this feedback type'))
+    } else {
+      res.send(utils.createSuccess(rows))
+    }
+  } catch (ex) {
+    res.send(utils.createError(ex))
+  }
+})
 module.exports = router
+
+
 
